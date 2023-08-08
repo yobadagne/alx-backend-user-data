d2bd0d325bbc5ca447c0687227a3e2b732b51a9e
yobadagne <yobadagne@gmail.com>
2023-08-08 11:47:34 +0300

    3

---
+++ api/v1/Auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Module of Authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Class to manage the API authentication """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method for validating if endpoint requires auth """
+        pass
+
+    def authorization_header(self, request=None) -> str:
+        """ Method that handles authorization header """
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Validates current user """
+        return None
