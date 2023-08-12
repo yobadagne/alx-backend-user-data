ed2d18a86ffd3258bf62a41d8f6f7ebfdaf480ae
yobadagne <yobadagne@gmail.com>
2023-08-12 14:52:53 +0000

    5

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" DocDocDocDocDocDoc
+"""
+Module
 """
 from flask import Blueprint
 
@@ -9,3 +10,5 @@ from api.v1.views.index import *
 from api.v1.views.users import *
 
 User.load_from_file()
+
+from api.v1.views.session_auth import 
