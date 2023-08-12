215c367dc0f6f2172f8a0c548d237ae287937fb6
yobadagne <yobadagne@gmail.com>
2023-08-12 15:04:08 +0000

    5

--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Module of Index views
+"""
+Module of Index views.
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -7,34 +8,38 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """GET /api/v1/status
     Return:
-      - the status of the API
+      - the status of the API.
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """GET /api/v1/stats
     Return:
-      - the number of each objects
+      - the number of each objects.
     """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """GET /api/v1/unauthorized
     Return:
-      - raises a 401 error by using abort
+      - Unauthorized error.
     """
     abort(401)
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """ GET /api/v1/forbidden
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
     Return:
-      - raises a 403 error by using abort
+      - Forbidden error.
     """
     abort(403)
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
- Users views.
+Module of Users views.
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
